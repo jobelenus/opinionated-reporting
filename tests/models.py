09e42385777bfa9de07275bbccad7bd24ce8f6cd
John Obelenus <jobelenus@gmail.com>
2018-09-15 15:48:10 -0400

    Fix app label problem, fix required on_delete

--- tests/models.py
+++ tests/models.py
@@ -7,20 +7,29 @@ class TestProduct(models.Model):
     name = models.CharField(max_length=256)
     price = models.DecimalField(max_digits=10, decimal_places=2)
 
+    class Meta:
+        app_label = 'tests'
+
 
 class TestCustomer(models.Model):
     email = models.EmailField(unique=True)
     name = models.CharField(max_length=256)
 
+    class Meta:
+        app_label = 'tests'
+
 
 class TestOrder(models.Model):
-    customer = models.ForeignKey(TestCustomer)
+    customer = models.ForeignKey(TestCustomer, on_delete=models.CASCADE)
     products = models.ManyToManyField(TestProduct, through='TestOrderItem')
     created_on = models.DateTimeField(auto_now_add=True)
     ordered_on = models.DateTimeField(null=True, blank=True, default=None)
     total = models.DecimalField(max_digits=10, decimal_places=2)
     tax = models.DecimalField(max_digits=10, decimal_places=2)
     cancelled = models.BooleanField(default=False)
+
+    class Meta:
+        app_label = 'tests'
 
 
 class TestOrderItem(models.Model):
@@ -29,38 +38,47 @@ class TestOrderItem(models.Model):
     quantity = models.PositiveIntegerField(default=1)
     total = models.DecimalField(max_digits=10, decimal_places=2)
 
+    class Meta:
+        app_label = 'tests'
+
 
 class CustomerDimension(BaseDimension):
 
     class ReportingMeta:
-        business_model = 'opinionated_reporting.tests.Customer'
+        business_model = TestCustomer
         unique_identifier = 'id'
         fields = ('name', 'email')
+
+    class Meta:
+        app_label = 'tests'
 
 
 class ProductDimension(BaseDimension):
 
     class ReportingMeta:
-        business_model = 'opinionated_reporting.tests.TestProduct'
+        business_model = TestProduct
         unique_identifier = 'id'
         fields = ('name', 'price')
 
+    class Meta:
+        app_label = 'tests'
+
 
 class OrderedProductFact(BaseFact):
-    customer = DimensionForeignKey(CustomerDimension)
-    product = DimensionForeignKey(ProductDimension)
+    customer = DimensionForeignKey(CustomerDimension, on_delete=models.CASCADE)
+    product = DimensionForeignKey(ProductDimension, on_delete=models.CASCADE)
     order_id = IntegerDescriptionField(computed=lambda instance: instance.order.id)
-    created_on = DimensionForeignKey(DateDimension, computed=lambda instance: instance.order.created_on, related_name="ordered_created_on")
-    hour_created_on = DimensionForeignKey(HourDimension, computed=lambda instance: instance.order.created_on, related_name="ordered_hour_created_on")
-    ordered_on = DimensionForeignKey(DateDimension, computed=lambda instance: instance.order.ordered_on, related_name="ordered_ordered_on")
-    hour_ordered_on = DimensionForeignKey(HourDimension, computed=lambda instance: instance.order.ordered_on, related_name="ordered_hour_ordered_on")
+    created_on = DimensionForeignKey(DateDimension, computed=lambda instance: instance.order.created_on, related_name="ordered_created_on", on_delete=models.CASCADE)
+    hour_created_on = DimensionForeignKey(HourDimension, computed=lambda instance: instance.order.created_on, related_name="ordered_hour_created_on", on_delete=models.CASCADE)
+    ordered_on = DimensionForeignKey(DateDimension, computed=lambda instance: instance.order.ordered_on, related_name="ordered_ordered_on", on_delete=models.CASCADE)
+    hour_ordered_on = DimensionForeignKey(HourDimension, computed=lambda instance: instance.order.ordered_on, related_name="ordered_hour_ordered_on", on_delete=models.CASCADE)
 
     @classmethod
     def delete_when(cls, instance):
         return instance.order.cancelled
 
     class ReportingMeta:
-        business_model = 'opinionated_reporting.tests.TestOrderItem'
+        business_model = TestOrderItem
         unique_identifier = 'id'
         fields = ('product', 'qty', 'total', 'order_id', 'customer', 'created_on', 'hour_created_on', 'hour_ordered_on', 'ordered_on')
         header_description = ['ID', 'Product', 'Qty', 'Total', 'Order ID''Created Date', 'Created Time', 'Customer', 'Ordered Date', 'Ordered Time']
@@ -77,20 +95,23 @@ class OrderedProductFact(BaseFact):
             row.hour_ordered_on.time,
         ]
 
+    class Meta:
+        app_label = 'tests'
+
 
 class OrderedFact(BaseFact):
-    customer = DimensionForeignKey(CustomerDimension)
-    created_on = DimensionForeignKey(DateDimension, related_name="ordered_created_on")
-    hour_reated_on = DimensionForeignKey(HourDimension, alias='created_on', related_name="ordered_hour_created_on")
-    ordered_on = DimensionForeignKey(DateDimension, related_name="ordered_ordered_on")
-    hour_ordered_on = DimensionForeignKey(HourDimension, alias='ordered_on', related_name="ordered_hour_ordered_on")
+    customer = DimensionForeignKey(CustomerDimension, on_delete=models.CASCADE)
+    created_on = DimensionForeignKey(DateDimension, related_name="ordered_created_on", on_delete=models.CASCADE)
+    hour_reated_on = DimensionForeignKey(HourDimension, alias='created_on', related_name="ordered_hour_created_on", on_delete=models.CASCADE)
+    ordered_on = DimensionForeignKey(DateDimension, related_name="ordered_ordered_on", on_delete=models.CASCADE)
+    hour_ordered_on = DimensionForeignKey(HourDimension, alias='ordered_on', related_name="ordered_hour_ordered_on", on_delete=models.CASCADE)
 
     @classmethod
     def delete_when(cls, instance):
         return instance.cancelled
 
     class ReportingMeta:
-        business_model = 'opinionated_reporting.tests.TestOrder'
+        business_model = TestOrder
         unique_identifier = 'id'
         fields = ('customer', 'created_on', 'hour_created_on', 'hour_ordered_on', 'ordered_on')
         header_description = ['ID', 'Created Date', 'Created Time', 'Customer', 'Ordered Date', 'Ordered Time']
@@ -103,3 +124,6 @@ class OrderedFact(BaseFact):
             row.hour_ordered_on.time,
         ]
         total = lambda filter_instance: filter_instance.qs.aggregate(total_count=models.Count('id'))
+
+    class Meta:
+        app_label = 'tests'
--- tests/settings.py
+++ tests/settings.py
@@ -25,7 +25,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'opinionated_reporting'
+    'tests',
+    'opinionated_reporting',
 ]
 
 MIDDLEWARE = [
